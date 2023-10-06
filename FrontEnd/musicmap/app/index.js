54835c279d5480e924e97afd408a05c1edca9d1e
Adhel-Esteban1 <adhel.rivera2@upr.edu>
2023-10-06 18:49:12 -0400

    Add navigation bar icons, delete unnecesary files

--- FrontEnd/musicmap/app/index.js
+++ FrontEnd/musicmap/app/index.js
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Settings from "./pages/settings";
 import Notifications from "./pages/notifications";
@@ -6,7 +5,14 @@ import Map from "./pages/map";
 import Friends from "./pages/friends";
 import Profile from "./pages/profile";
 
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Entypo from '@expo/vector-icons/Entypo';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
 const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
     <Tab.Navigator
@@ -15,11 +21,36 @@ const App = () => {
         tabBarShowLabel: false,
       }}
     >
-      <Tab.Screen name="Settings" component={Settings} />
-      <Tab.Screen name="Friends" component={Friends} />
-      <Tab.Screen name="Map" component={Map} />
-      <Tab.Screen name="Notifications" component={Notifications} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Settings" component={Settings} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="gear" size={"30%"} color={color} />
+            
+          ),
+        }} />
+
+      <Tab.Screen name="Friends" component={Friends} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="user-friends" size={"30%"} color={color} />
+          ),
+        }} />
+
+      <Tab.Screen name="MusicMap" component={Map} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="location-pin" size={"37%"} color={color} />
+          ),
+        }} />
+
+      <Tab.Screen name="Notifications" component={Notifications} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="bell" size={"30%"} color={color} />
+          ),
+        }} />
+
+      <Tab.Screen name="Profile" component={Profile} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={"30%"} color={color} />
+          ),
+        }} />
     </Tab.Navigator>
   );
 };
