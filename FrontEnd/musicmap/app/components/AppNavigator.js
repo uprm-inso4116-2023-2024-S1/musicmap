8901263623b30d66705af2fb1a13fc1ac4e9981c
Axel Negron <axel.negron2@upr.edu>
2023-10-12 23:12:34 -0400

    Account tab implementation

--- FrontEnd/musicmap/app/components/AppNavigator.js
+++ FrontEnd/musicmap/app/components/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Account from '../pages/account';
 import Map from '../pages/map'
 
 const { Navigator, Screen } = createBottomTabNavigator()
@@ -9,8 +9,8 @@ const { Navigator, Screen } = createBottomTabNavigator()
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-        <Navigator headerMode='none' initialRouteName='Map'>
-            <Screen name="Map" component={Map}/>
+        <Navigator headerMode='none' initialRouteName='Account'>
+            <Screen name="Account" component={Account}/>
         </Navigator>
     </NavigationContainer>
   )
--- FrontEnd/musicmap/app/index.js
+++ FrontEnd/musicmap/app/index.js
@@ -5,6 +5,7 @@ import Notifications from "./pages/notifications";
 import Map from "./pages/map";
 import Friends from "./pages/friends";
 import Profile from "./pages/profile";
+import Account from "./pages/account";
 
 const Tab = createBottomTabNavigator();
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
       <Tab.Screen name="Map" component={Map} />
       <Tab.Screen name="Notifications" component={Notifications} />
       <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Account" component={Account} />
     </Tab.Navigator>
   );
 };
