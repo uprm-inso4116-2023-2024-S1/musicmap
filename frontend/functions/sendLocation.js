7bb6d26225dfc0b6b8df07248a561103eaa3a777
AngelGonzs <angel.gonzalez49@upr.edu>
2023-10-19 15:46:05 -0400

    Instructions for axios url

--- frontend/functions/sendLocation.js
+++ frontend/functions/sendLocation.js
@@ -13,6 +13,8 @@ async function sendLocation(location) {
     // Using Axios for making a POST request. We're just
     // sending the location from the Front End to Back End.
     
+    // Put your development server (expo server) IP Address here
+    // alongside the port you're using e.g. 192.0.0.0:5000
     axios.post(`${url}/maps/location`, location)
         .then(response => {
             console.log(response.data.message); // Message from the server
