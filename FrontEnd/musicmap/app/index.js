2dbe5f0095f7ffdd44f54088a472c937c008cb57
Axel Negron <axel.negron2@upr.edu>
2023-10-20 09:43:52 -0400

    Added submenus and updated account section.

--- FrontEnd/musicmap/app/index.js
+++ FrontEnd/musicmap/app/index.js
@@ -4,13 +4,16 @@ import Notifications from "./pages/notifications";
 import Map from "./pages/map";
 import Friends from "./pages/friends";
 import Profile from "./pages/profile";
-
+import Account from "./pages/account";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Entypo from '@expo/vector-icons/Entypo';
 import Ionicons from '@expo/vector-icons/Ionicons';
-
-
+import Profile_settings from "./pages/profile_settings";
+import Notification_settings from "./pages/notification_settings";
+import Language_settings from "./pages/language_settings";
+import Privacy_settings from "./pages/privacy_settings";
+import FAQSection from "./pages/FAQ_settings";
 const Tab = createBottomTabNavigator();
 
 const App = () => {
@@ -52,6 +55,28 @@ const App = () => {
             <Ionicons name="person" size={"30%"} color={color} />
           ),
         }} />
+
+      <Tab.Screen name="Account" component={Account}  options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={"30%"} color={color} />
+          ),
+        }} />
+      <Tab.Screen name="Profile_settings" component={Profile_settings}  options={{tabBarButton: () => null,
+        tabBarVisible:false //hide tab bar on this screen
+        }} />
+      <Tab.Screen name="Notification_settings" component={Notification_settings}  options={{tabBarButton: () => null,
+        tabBarVisible:false //hide tab bar on this screen
+        }} />
+      <Tab.Screen name="Language_settings" component={Language_settings}  options={{tabBarButton: () => null,
+        tabBarVisible:false //hide tab bar on this screen
+        }} />
+      <Tab.Screen name="Privacy_settings" component={Privacy_settings}  options={{tabBarButton: () => null,
+        tabBarVisible:false //hide tab bar on this screen
+        }} />
+      <Tab.Screen name="FAQSection" component={FAQSection}  options={{tabBarButton: () => null,
+        tabBarVisible:false //hide tab bar on this screen
+        }} />
+
     </Tab.Navigator>
   );
 };
---
+++ FrontEnd/musicmap/app/pages/account.js
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Stack } from 'expo-router';
+
+const Account = () => {
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const handleLogin = () => {
+    //Function to call when log-in is pressed
+    if ((userName == "") & (userPassword == "")) {
+      console.log("Empty Fields");
+    } else {
+      console.log("Username:", userName);
+      console.log("Password:", userPassword);
+    }
+  };
+
+  const handleforgot = () => {
+    //Function to call when Forgot is pressed
+    console.log("Forgot Password");
+  };
+  const handlesignup = () => {
+    //Function to call when Sign up is pressed
+    console.log("Sign-Up");
+  };
+
+  return (
+    <ScrollView>
+      <KeyboardAvoidingView>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.title}>MusicMap</Text>
+          <View style={styles.loginContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Username or Email"
+              value={userName}
+              onChangeText={(text) => setUserName(text)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              value={userPassword}
+              onChangeText={(text) => setUserPassword(text)}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+              <Text style={styles.buttonText}>Log-in</Text>
+            </TouchableOpacity>
+
+            <View style={styles.bottomtxt}>
+              <TouchableOpacity onPress={handlesignup}>
+                <Text style={styles.bottomText}>
+                  Don’t have an account? Sign Up
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleforgot}>
+                <Text style={styles.bottomText}>Forgot Password</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
+      <Stack.Screen //Allows editing of screen components, header, footer, etc
+            options={{
+              headerLeft: () => (
+                <Text
+                  style={{
+                    color: "#424242",
+                    fontSize: 25,
+                    marginLeft: "7%",
+                    fontFamily: "System",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Account_Login
+                </Text>
+              ),
+              headerTitle: "",
+            }}
+          />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start", // Aligns content at the top of the screen
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 20, // Add top margin to title
+    marginBottom: 150,
+    color: "#424242",
+  },
+  loginContainer: {
+    width: "80%",
+    alignItems: "center", // Center the login fields horizontally
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+    width: "100%", // Make the input fields full width
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: "#D10050",
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+    width: "100%", // Make the button full width
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  bottomtxt: {
+    margin: 10,
+    alignContent: "center",
+  },
+  bottomText: {
+    color: "#D10050",
+    padding: 2,
+    paddingTop: 5,
+    textAlign: "center",
+  },
+});
+
+export default Account;
