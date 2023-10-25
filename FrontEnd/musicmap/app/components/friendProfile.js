3c9c18feee864c1c767e8a854c1139d57f03ec7c
Adhel-Esteban <93483234+Adhel-Esteban1@users.noreply.github.com>
2023-10-25 19:25:58 -0400

    Refactor styles in friendProfile.js
    Refactor the styles in friendProfile.js to look more similar to the other files.

--- FrontEnd/musicmap/app/components/friendProfile.js
+++ FrontEnd/musicmap/app/components/friendProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 import Widget from './widget';
@@ -14,45 +14,9 @@ function FriendProfile() {
     >
       <ScrollView showsVerticalScrollIndicator={false}>
       
-        <TouchableOpacity
-          style={{
-            height: 106, //60%',
-            width: "90%", //width of designated area relative to screen size
-            justifyContent: "center", //Literally places all objects in the center of the area
-            backgroundColor: "white", //Sets the color to the color code input, can also be place literally, like 'red'
-            marginLeft: "5%",
-            borderRadius: 10, //Rounds the corners of the imaginary box area
-            elevation: 5,
-            marginBottom: 18,
-          }}
-        >
-          <Text
-            style={{
-              color: "#424242",
-              textAlign: "right",
-              marginRight: 20,
-              fontSize: 25,
-              marginLeft: "3%",
-              marginTop: "-10%",
-              fontFamily: "System",
-              fontWeight: "bold",
-            }}
-          >
-            Friend Name
-          </Text>
-          <Text
-            style={{
-              color: "#424242",
-              textAlign: "right",
-              marginRight: 20,
-              fontSize: 15,
-              marginLeft: "3%",
-              fontFamily: "System",
-              fontWeight: "bold",
-            }}
-          >
-            Bio 
-          </Text>
+        <TouchableOpacity style={styles.titleCard}>
+          <Text style={styles.titleText}>Friend Name</Text>
+          <Text style={styles.bioText}>Bio </Text>
         </TouchableOpacity>
 
         <Widget
@@ -66,30 +30,66 @@ function FriendProfile() {
         <Widget
         MainText={'Favorite Aritsts'}
         SecondText={"Friend's favorite artists"}
-        />
-          
+        />   
  
       </ScrollView>
 
       <Stack.Screen //Allows editing of screen components, header, footer, etc
         options={{
           headerLeft: () => (
-            <Text
-              style={{
-                color: "#424242",
-                fontSize: 24,
-                marginLeft: "7%",
-                fontFamily: "System",
-                fontWeight: "bold",
-              }}
-            >
-              Friend Profile
-            </Text>
+            <Text style={styles.headerStyle}>Friend Profile</Text>
           ),
+          headerTitle: "",
         }}
       />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  headerStyle:{
+    color: "#424242",
+    fontSize: 24,
+    marginLeft: "7%",
+    fontFamily: "System",
+    fontWeight: "bold",
+  },
+  titleCard: {
+    height: 106, 
+    width: "90%", 
+    justifyContent: "center", 
+    backgroundColor: "white", 
+    marginLeft: "5%",
+    borderRadius: 10,
+    elevation: 5,
+    marginBottom: 18,
+  },
+  titleText: {
+    color: "#424242",
+    textAlign: "right",
+    marginRight: 20,
+    fontSize: 25,
+    marginLeft: "3%",
+    marginTop: "-10%",
+    fontFamily: "System",
+    fontWeight: "bold",
+  },
+  bioText: {
+    color: "#424242",
+    textAlign: "right",
+    marginRight: 20,
+    fontSize: 15,
+    marginLeft: "3%",
+    fontFamily: "System",
+    fontWeight: "bold",
+  },
+  secondText: {
+    color: "#424242",
+    textAlign: "left",
+    marginLeft: 10,
+    fontSize: 15,
+    fontFamily: "System",
+  },
+});
 
 export default FriendProfile;
