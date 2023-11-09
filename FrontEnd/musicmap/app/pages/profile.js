42fbcfe556b4ecfa3085999e4f987aee2ab40af2
insert GitHub username <insert GitHub email here>
2023-11-09 19:10:30 -0400

    Added the bio feature

--- FrontEnd/musicmap/app/pages/profile.js
+++ FrontEnd/musicmap/app/pages/profile.js
@@ -1,5 +1,4 @@
 // Import necessary React and React Native components
-
 import React, { useState } from "react";
 import { ScrollView, Text, TouchableOpacity, View, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -30,6 +29,7 @@ const Profile = () => {
     },
   ]);
 
+  // States for handling editing functionality
   const [isEditingSubtext, setIsEditingSubtext] = useState(false);
   const [editedSubtext, setEditedSubtext] = useState("");
   const [currentIndex, setCurrentIndex] = useState(-1);
@@ -45,7 +45,7 @@ const Profile = () => {
     setEditedSubtext(subtext);
   };
 
-   // Function to update subtext when editing is complete
+  // Function to update subtext when editing is complete
   const updateSubtext = () => {
     if (currentIndex !== -1) {
       // Create a copy of widgetData and update the edited subtext
@@ -57,86 +57,112 @@ const Profile = () => {
     setCurrentIndex(-1);
   };
 
+  // Function to dynamically set styles for the "User Name" widget
+  const getUserNameWidgetStyle = (index) => {
+    return {
+      height: widgetData[index].title === "User Name" ? 212 : 106,
+      width: "90%",
+      backgroundColor: "white",
+      marginLeft: "5%",
+      borderRadius: 10,
+      elevation: 5,
+      marginBottom: 18,
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      borderWidth: widgetData[index].title === "User Name" ? 2 : 0,
+      borderColor: "blue",
+    };
+  };
+
+  // Function to render the "User Name" widget
+  const renderUserNameWidget = (widget, index) => {
+    const isUserNameWidget = widget.title === "User Name";
+
+    return (
+      <TouchableOpacity
+        key={widget.title}
+        onPress={() => toggleEditSubtext(widget.subtext, index)}
+        style={getUserNameWidgetStyle(index)}
+      >
+        {isEditingSubtext && currentIndex === index ? (
+          <>
+            {/* Text input for editing the bio */}
+            <TextInput
+              value={editedSubtext}
+              onChangeText={(text) => setEditedSubtext(text)}
+              onBlur={updateSubtext}
+              placeholder="Enter your bio here"
+              multiline
+              style={{
+                marginLeft: "3%",
+                fontSize: 15,
+                fontFamily: "System",
+                fontWeight: "bold",
+                textAlign: "left",
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <View style={{ flex: 1 }}>
+              <Text
+                style={{
+                  color: "#424242",
+                  fontSize: 25,
+                  marginLeft: "3%",
+                  fontFamily: "System",
+                  fontWeight: "bold",
+                  textAlign: "left",
+                }}
+              >
+                {widget.title}
+              </Text>
+              {isUserNameWidget ? (
+                // Styling for the special "User Name" widget
+                <View
+                  style={{
+                    marginLeft: "3%",
+                    marginTop: 10,
+                    width: 75,
+                    height: 75,
+                    backgroundColor: "gray",
+                    borderRadius: 50,
+                  }}
+                ></View>
+              ) : (
+                // Styling for other widgets
+                <View
+                  style={{
+                    width: "100%",
+                    height: 5,
+                    backgroundColor: "#424242",
+                  }}
+                ></View>
+              )}
+              <Text
+                style={{
+                  color: "#424242",
+                  fontSize: 15,
+                  marginLeft: "3%",
+                  fontFamily: "System",
+                  fontWeight: "bold",
+                  textAlign: "left",
+                }}
+              >
+                {widget.subtext}
+              </Text>
+            </View>
+          </>
+        )}
+      </TouchableOpacity>
+    );
+  };
+
   // Render the Profile component
   return (
     <SafeAreaView style={{ flexDirection: "row", marginTop: 10 }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        {widgetData.map((widget, index) => (
-          <TouchableOpacity
-            key={widget.title}
-            onPress={() => toggleEditSubtext(widget.subtext, index)}
-            style={{
-              height: widget.title === "User Name" ? 212 : 106,
-              width: "90%",
-              backgroundColor: "white",
-              marginLeft: "5%",
-              borderRadius: 10,
-              elevation: 5,
-              marginBottom: 18,
-              flexDirection: "row",
-              justifyContent: "flex-start",
-            }}
-          >
-            {isEditingSubtext && currentIndex === index ? (
-              <>
-                <TextInput
-                  value={editedSubtext}
-                  onChangeText={(text) => setEditedSubtext(text)}
-                  onBlur={updateSubtext}
-                />
-              </>
-            ) : (
-              <>
-                <View style={{ flex: 1 }}>
-                  <Text
-                    style={{
-                      color: "#424242",
-                      fontSize: 25,
-                      marginLeft: "3%",
-                      fontFamily: "System",
-                      fontWeight: "bold",
-                      textAlign: "left",
-                    }}
-                  >
-                    {widget.title}
-                  </Text>
-                  {widget.title === "User Name" ? (
-                    <View
-                      style={{
-                        marginLeft: "3%",
-                        marginTop: 10,
-                        width: 75, // Increase width to make the circle bigger
-                        height: 75, // Increase height to make the circle bigger
-                        backgroundColor: "gray",
-                        borderRadius: 50, // Make it a bigger circle
-                      }}
-                    ></View>
-                  ) : (
-                    <View
-                      style={{
-                        width: "100%",
-                        height: 5,
-                        backgroundColor: "#424242",
-                      }}
-                    ></View>
-                  )}
-                  <Text
-                    style={{
-                      color: "#424242",
-                      fontSize: 15,
-                      marginLeft: "3%",
-                      fontFamily: "System",
-                      fontWeight: "bold",
-                      textAlign: "left",
-                    }}
-                  >
-                    {widget.subtext}
-                  </Text>
-                </View>
-              </>
-            )}
-          </TouchableOpacity>
-        ))}
+        {widgetData.map((widget, index) => renderUserNameWidget(widget, index))}
       </ScrollView>
     </SafeAreaView>
   );
