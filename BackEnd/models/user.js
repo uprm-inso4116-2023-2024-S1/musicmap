f7b1d691bbacc6174582a62ac9f70e877c4a12e3
AngelGonzs <angel.gonzalez49@upr.edu>
2023-11-26 00:32:04 -0400

    Forgot to update this part.

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -31,7 +31,4 @@ const UserSchema = new mongoose.Schema({
       }
 
 });
-
-const User = mongoose.model('User', UserSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
